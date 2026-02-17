be9c895be2b741ff003ee4f0daa0831d34c7a407
Qian Qi <qianqi@MacBook-Pro.local>
2026-02-17 15:26:28 +0800

    refactor(workload): disable direct unregister in favor of unbonding flow

--- chain/x/workload/keeper/msg_server_unregister_worker.go
+++ chain/x/workload/keeper/msg_server_unregister_worker.go
@@ -4,32 +4,15 @@ import (
 	"context"
 
 	"chain/x/workload/types"
-	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) UnregisterWorker(goCtx context.Context, msg *types.MsgUnregisterWorker) (*types.MsgUnregisterWorkerResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
+	_ = sdk.UnwrapSDKContext(goCtx)
+	_ = msg
 
-	worker, found := k.GetWorker(ctx, msg.Creator)
-	if !found {
-		return nil, sdkerrors.ErrKeyNotFound.Wrap("worker not found")
-	}
-
-	creator, err := sdk.AccAddressFromBech32(msg.Creator)
-	if err != nil {
-		return nil, err
-	}
-
-	if worker.Stake > 0 {
-		coins := sdk.NewCoins(sdk.NewCoin("stake", math.NewIntFromUint64(worker.Stake)))
-		if err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, creator, coins); err != nil {
-			return nil, err
-		}
-	}
-
-	k.RemoveWorker(ctx, msg.Creator)
-
-	return &types.MsgUnregisterWorkerResponse{}, nil
+	// Deprecated path: direct unregister/withdraw is disabled to enforce cooldown safety.
+	// Use `request-unbonding` and then `finalize-unbonding` after release height.
+	return nil, sdkerrors.ErrInvalidRequest.Wrap("direct unregister disabled; use request-unbonding then finalize-unbonding")
 }
