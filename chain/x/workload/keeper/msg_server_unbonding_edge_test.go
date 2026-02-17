9cb3582fd6b00a9ebfe1b8d46125650243fba395
Qian Qi <qianqi@MacBook-Pro.local>
2026-02-17 16:11:31 +0800

    test(workload): cover unbonding extend/finalize boundary scenarios

---
+++ chain/x/workload/keeper/msg_server_unbonding_edge_test.go
@@ -0,0 +1,96 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"chain/testutil/sample"
+	"chain/x/workload/keeper"
+	"chain/x/workload/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExtendUnbonding_Edges(t *testing.T) {
+	t.Run("extra blocks zero", func(t *testing.T) {
+		k, srv, ctx := setupMsgServer(t)
+		wctx := sdk.UnwrapSDKContext(ctx)
+		worker := sample.AccAddress()
+		k.SetUnbonding(wctx, types.Unbonding{Creator: worker, ReleaseHeight: 100, Amount: 100000})
+
+		_, err := srv.ExtendUnbonding(wctx, &types.MsgExtendUnbonding{
+			Creator:     k.GetAuthority(),
+			Worker:      worker,
+			ExtraBlocks: 0,
+		})
+		require.ErrorIs(t, err, types.ErrInvalidExtraBlocks)
+	})
+
+	t.Run("extra blocks exceeds max", func(t *testing.T) {
+		k, srv, ctx := setupMsgServer(t)
+		wctx := sdk.UnwrapSDKContext(ctx)
+		worker := sample.AccAddress()
+		k.SetUnbonding(wctx, types.Unbonding{Creator: worker, ReleaseHeight: 100, Amount: 100000})
+
+		_, err := srv.ExtendUnbonding(wctx, &types.MsgExtendUnbonding{
+			Creator:     k.GetAuthority(),
+			Worker:      worker,
+			ExtraBlocks: keeper.MaxExtraUnbondingBlocks + 1,
+		})
+		require.ErrorIs(t, err, types.ErrInvalidExtraBlocks)
+	})
+
+	t.Run("unbonding not found", func(t *testing.T) {
+		k, srv, ctx := setupMsgServer(t)
+		wctx := sdk.UnwrapSDKContext(ctx)
+
+		_, err := srv.ExtendUnbonding(wctx, &types.MsgExtendUnbonding{
+			Creator:     k.GetAuthority(),
+			Worker:      sample.AccAddress(),
+			ExtraBlocks: 10,
+		})
+		require.ErrorIs(t, err, types.ErrUnbondingNotFound)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		k, srv, ctx := setupMsgServer(t)
+		wctx := sdk.UnwrapSDKContext(ctx)
+		worker := sample.AccAddress()
+		k.SetUnbonding(wctx, types.Unbonding{Creator: worker, ReleaseHeight: 100, Amount: 100000})
+
+		_, err := srv.ExtendUnbonding(wctx, &types.MsgExtendUnbonding{
+			Creator:     k.GetAuthority(),
+			Worker:      worker,
+			ExtraBlocks: 25,
+		})
+		require.NoError(t, err)
+
+		u, found := k.GetUnbonding(wctx, worker)
+		require.True(t, found)
+		require.Equal(t, uint64(125), u.ReleaseHeight)
+	})
+}
+
+func TestFinalizeUnbonding_HeightEdges(t *testing.T) {
+	k, srv, ctx := setupMsgServer(t)
+	worker := sample.AccAddress()
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	k.SetUnbonding(sdkCtx, types.Unbonding{
+		Creator:       worker,
+		ReleaseHeight: uint64(sdkCtx.BlockHeight()) + keeper.UnbondingPeriodBlocks,
+		Amount:        100000,
+	})
+
+	_, err := srv.FinalizeUnbonding(sdkCtx, &types.MsgFinalizeUnbonding{Creator: worker})
+	require.ErrorIs(t, err, types.ErrUnbondingCooldownNotReached)
+
+	sdkCtx = sdkCtx.WithBlockHeight(int64(keeper.UnbondingPeriodBlocks))
+	_, err = srv.FinalizeUnbonding(sdkCtx, &types.MsgFinalizeUnbonding{Creator: worker})
+	require.NoError(t, err)
+
+	_, found := k.GetUnbonding(sdkCtx, worker)
+	require.False(t, found)
+
+	_, err = srv.FinalizeUnbonding(sdkCtx, &types.MsgFinalizeUnbonding{Creator: worker})
+	require.ErrorIs(t, err, types.ErrUnbondingNotFound)
+}
