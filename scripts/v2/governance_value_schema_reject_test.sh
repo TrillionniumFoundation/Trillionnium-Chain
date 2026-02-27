1df70dba8d16f20b406b688ce6eb712615ef0b96
Qian Qi <qianqi@MacBook-Pro.local>
2026-02-27 16:19:10 +0800

    laneB: gate emergency_pause noop idempotency regression

--- scripts/v2/governance_value_schema_reject_test.sh
+++ scripts/v2/governance_value_schema_reject_test.sh
@@ -9,6 +9,7 @@ echo "[TEST] governance_value_schema_reject: invalid value should be rejected"
 cargo test -q -p trnm-state governance_param_schema_rejects_invalid_u64_values -- --nocapture
 cargo test -q -p trnm-state emergency_pause_requires_strict_bool_literal -- --nocapture
 cargo test -q -p trnm-state emergency_pause_checked_path_is_immediate_and_non_cancellable -- --nocapture
+cargo test -q -p trnm-state emergency_pause_checked_noop_update_is_idempotent_after_pause -- --nocapture
 cargo test -q -p trnm-state emergency_pause_checked_path_rejects_non_canonical_key_id -- --nocapture
 cargo test -q -p trnm-state emergency_pause_checked_path_key_id_validation_precedes_bool_schema_validation -- --nocapture
 cargo test -q -p trnm-state emergency_pause_does_not_bypass_sensitive_timelock_guards -- --nocapture
