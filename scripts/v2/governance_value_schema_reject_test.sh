bef9087c461d171eb39588882ec1acb331bf425c
Qian Qi <qianqi@MacBook-Pro.local>
2026-02-27 14:41:57 +0800

    dev(gov): pin emergency pause key-id error precedence

--- scripts/v2/governance_value_schema_reject_test.sh
+++ scripts/v2/governance_value_schema_reject_test.sh
@@ -10,5 +10,6 @@ cargo test -q -p trnm-state governance_param_schema_rejects_invalid_u64_values -
 cargo test -q -p trnm-state emergency_pause_requires_strict_bool_literal -- --nocapture
 cargo test -q -p trnm-state emergency_pause_checked_path_is_immediate_and_non_cancellable -- --nocapture
 cargo test -q -p trnm-state emergency_pause_checked_path_rejects_non_canonical_key_id -- --nocapture
+cargo test -q -p trnm-state emergency_pause_checked_path_key_id_validation_precedes_bool_schema_validation -- --nocapture
 
 echo "[OK] governance_value_schema_reject passed"
