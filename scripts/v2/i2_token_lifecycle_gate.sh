4ca7272116354b6e45646fc9fba040df753b8bbe
Qian Qi <qianqi@MacBook-Pro.local>
2026-02-28 21:54:24 +0800

    laneXI: gate DID replay older-height rejection in I2 lifecycle

--- scripts/v2/i2_token_lifecycle_gate.sh
+++ scripts/v2/i2_token_lifecycle_gate.sh
@@ -10,6 +10,7 @@ cargo test -p trnm-types issue_capability_rejects_height_before_did_creation_wit
 cargo test -p trnm-types renew_capability_extends_expiry_and_appends_audit
 cargo test -p trnm-types revoke_capability_replay_with_older_height_is_rejected_without_side_effects
 cargo test -p trnm-types revoke_did_replay_repairs_legacy_uncascaded_capability_without_rewriting_did_timestamp
+cargo test -p trnm-types revoke_did_replay_with_older_height_is_rejected_without_side_effects
 cargo test -p trnm-types verify_capability_accepts_active_controller_and_matching_scope
 
 echo "[I2][PASS] capability token lifecycle gate"
