5eccc19a4d23d53af29decca0bb99b4efa255ba1
Qian Qi <qianqi@MacBook-Pro.local>
2026-03-02 03:14:06 +0800

    laneXI: gate issue-height revocation boundary regression

--- scripts/v2/i2_token_lifecycle_gate.sh
+++ scripts/v2/i2_token_lifecycle_gate.sh
@@ -30,6 +30,7 @@ cargo test -p trnm-types verify_capability_rejects_scope_mismatch_without_side_e
 cargo test -p trnm-types verify_capability_rejects_unknown_token_without_side_effects
 cargo test -p trnm-types verify_capability_rejects_expired_token_without_side_effects
 cargo test -p trnm-types capability_revocation_takes_precedence_over_expiry_window
+cargo test -p trnm-types capability_revoked_at_issue_height_is_never_active
 cargo test -p trnm-types verify_capability_accepts_height_equal_to_expiry_boundary
 cargo test -p trnm-types verify_capability_rejects_revoked_did_even_if_token_looks_active
 cargo test -p trnm-types verify_capability_allows_historical_height_before_did_revocation
