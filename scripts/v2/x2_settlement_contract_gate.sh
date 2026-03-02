04a12030a63801079bf7e56596477d44e3b1171f
Qian Qi <qianqi@MacBook-Pro.local>
2026-03-03 07:16:54 +0800

    laneXI: gate degraded heartbeat precedence in X2 settlement contract

--- scripts/v2/x2_settlement_contract_gate.sh
+++ scripts/v2/x2_settlement_contract_gate.sh
@@ -40,6 +40,10 @@ cargo test -p trnm-bridge-poc --test x2_settlement_loop \
   -- --nocapture
 
 cargo test -p trnm-bridge-poc --test x2_settlement_loop \
+  x3_prep_degraded_heartbeat_takes_precedence_over_timeout_confirm_failure \
+  -- --nocapture
+
+cargo test -p trnm-bridge-poc --test x2_settlement_loop \
   x3_prep_manual_degraded_blank_message_uses_stable_failure_fallback \
   -- --nocapture
 
