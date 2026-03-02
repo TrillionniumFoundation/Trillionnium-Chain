399510340ee2e62e5445abf964400f204e498f57
Qian Qi <qianqi@MacBook-Pro.local>
2026-03-03 03:17:44 +0800

    laneXI: gate verify fail-closed at same-height renew/revoke boundary

--- scripts/v2/i2_token_lifecycle_gate.sh
+++ scripts/v2/i2_token_lifecycle_gate.sh
@@ -70,6 +70,8 @@ cargo test -p trnm-types --test i3_capability_issue_renew_revoke_competition \
   revoke_dominates_issue_renew_revoke_competition_at_same_height
 cargo test -p trnm-types --test i3_capability_issue_renew_revoke_competition \
   renew_at_revocation_boundary_is_fail_closed_when_revoke_lands_first
+cargo test -p trnm-types --test i3_capability_issue_renew_revoke_competition \
+  verify_at_revocation_boundary_is_fail_closed_after_same_height_renew_revoke_race
 cargo test -p trnm-types --test i3_capability_nonexpiring_revoke_competition \
   nonexpiring_token_revoke_then_same_height_renew_is_fail_closed
 
