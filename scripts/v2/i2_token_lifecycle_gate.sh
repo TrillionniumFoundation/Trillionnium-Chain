d78af94e3db65404b2ed2f4cee4bb1a68a6e6111
Qian Qi <qianqi@MacBook-Pro.local>
2026-03-03 09:49:10 +0800

    laneXI: extend I3 fail-closed precedence coverage in i2 gate

--- scripts/v2/i2_token_lifecycle_gate.sh
+++ scripts/v2/i2_token_lifecycle_gate.sh
@@ -86,5 +86,9 @@ cargo test -p trnm-types --test i3_capability_fail_closed_precedence \
   revoked_token_with_scope_mismatch_returns_inactive_fail_closed
 cargo test -p trnm-types --test i3_capability_fail_closed_precedence \
   revoked_token_with_unauthorized_actor_returns_inactive_fail_closed
+cargo test -p trnm-types --test i3_capability_fail_closed_precedence \
+  expired_token_with_scope_mismatch_returns_inactive_fail_closed
+cargo test -p trnm-types --test i3_capability_fail_closed_precedence \
+  preissued_token_with_unauthorized_actor_returns_inactive_fail_closed
 
 echo "[I2][PASS] capability token lifecycle gate"
