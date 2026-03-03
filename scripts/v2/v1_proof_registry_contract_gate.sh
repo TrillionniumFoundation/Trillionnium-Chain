d4cee0259996c790e9cf4a71c6bee420ee1d9aec
Qian Qi <qianqi@MacBook-Pro.local>
2026-03-03 17:02:41 +0800

    laneMV: extend V1 proof registry gate with TEE alias coverage

--- scripts/v2/v1_proof_registry_contract_gate.sh
+++ scripts/v2/v1_proof_registry_contract_gate.sh
@@ -10,6 +10,8 @@ cargo test -p trnm-pouw registry_is_registered_for_reports_true_for_builtin_stac
 cargo test -p trnm-pouw registry_aliases_stay_aligned_with_receipt_normalization_contract
 cargo test -p trnm-pouw registry_is_registered_kind_accepts_version_suffixed_legacy_aliases
 cargo test -p trnm-pouw registry_is_registered_kind_accepts_punctuated_legacy_receipt_aliases
+cargo test -p trnm-pouw registry_is_registered_kind_accepts_dcap_quote_aliases
+cargo test -p trnm-pouw registry_is_registered_kind_accepts_tdx_and_sev_snp_aliases
 cargo test -p trnm-pouw registry_is_registered_kind_accepts_fullwidth_punctuation_aliases
 cargo test -p trnm-pouw registry_is_registered_kind_accepts_zero_width_separated_aliases
 cargo test -p trnm-pouw registry_is_registered_kind_accepts_non_breaking_space_separated_aliases
