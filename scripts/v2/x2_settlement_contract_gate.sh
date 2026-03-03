91980d5a33efe646c40fa1040e08471d56369095
Qian Qi <qianqi@MacBook-Pro.local>
2026-03-03 09:06:24 +0800

    laneXI: anchor older-height reorder confirm in x2 gate matrix

--- scripts/v2/x2_settlement_contract_gate.sh
+++ scripts/v2/x2_settlement_contract_gate.sh
@@ -20,6 +20,10 @@ cargo test -p trnm-bridge-poc --test x2_settlement_loop \
   -- --nocapture
 
 cargo test -p trnm-bridge-poc --test x2_settlement_loop \
+  x3_prep_reorder_confirm_with_older_height_after_finalize_is_rejected_without_state_change \
+  -- --nocapture
+
+cargo test -p trnm-bridge-poc --test x2_settlement_loop \
   x3_prep_reorder_failed_confirm_after_finalize_is_rejected_without_state_change \
   -- --nocapture
 
--- scripts/v2/x2_settlement_contract_gate_fault_matrix_coverage_test.sh
+++ scripts/v2/x2_settlement_contract_gate_fault_matrix_coverage_test.sh
@@ -7,6 +7,7 @@ GATE="$ROOT/scripts/v2/x2_settlement_contract_gate.sh"
 required_tests=(
   "x2_failure_path_confirm_failed_triggers_compensation_revert"
   "x3_prep_duplicate_confirm_after_finalize_is_rejected_without_state_change"
+  "x3_prep_reorder_confirm_with_older_height_after_finalize_is_rejected_without_state_change"
   "x3_prep_reorder_failed_confirm_after_finalize_is_rejected_without_state_change"
   "x3_prep_stale_pending_on_degraded_heartbeat_triggers_compensation_revert"
   "x3_prep_degraded_heartbeat_takes_precedence_over_timeout_confirm_failure"
