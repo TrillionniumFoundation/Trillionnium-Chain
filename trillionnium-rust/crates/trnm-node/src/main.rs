026ca9f6fc70ae183439558fd12d522b91504f4e
Qian Qi <qianqi@MacBook-Pro.local>
2026-03-12 02:00:58 +0800

    lane-L04: add rollback block rate ppm guardrail

--- trillionnium-rust/crates/trnm-node/src/main.rs
+++ trillionnium-rust/crates/trnm-node/src/main.rs
@@ -2799,14 +2799,26 @@ mod tests {
     }
 
     #[test]
+    fn preexec_peak_share_metric_makes_tail_latency_regressions_visible() {
+        let finality_max = 320u128;
+        let preexec_max = 160u128;
+
+        assert_eq!(ratio_ppm(preexec_max, finality_max), 500_000);
+        assert_eq!(ratio_ppm(preexec_max, 0), 0);
+    }
+
+    #[test]
     fn rollback_share_metrics_make_rollback_regressions_visible() {
         let finality_avg = 200u128;
         let rollback_avg = 40u128;
         let finality_max = 320u128;
         let rollback_max = 80u128;
+        let rollback_block_total = 2u64;
+        let finality_sample_count = 4u64;
 
         assert_eq!(ratio_ppm(rollback_avg, finality_avg), 200_000);
         assert_eq!(ratio_ppm(rollback_max, finality_max), 250_000);
+        assert_eq!(ratio_ppm_u64(rollback_block_total, finality_sample_count), 500_000);
     }
 
     #[test]
@@ -5486,6 +5498,7 @@ fn main() -> Result<()> {
     let state_root_total_share_avg_ppm = ratio_ppm(state_root_total_avg, finality_avg);
     let rollback_share_avg_ppm = ratio_ppm(rollback_avg, finality_avg);
     let rollback_peak_share_ppm = ratio_ppm(rollback_max, finality_max);
+    let rollback_block_rate_ppm = ratio_ppm_u64(rollback_block_total, finality_samples_ms.len() as u64);
     let preexec_conflict_miss_share_bps =
         ratio_percent_bps(apply_error_preexec_conflict_miss_total as u128, preexec_reject_total as u128);
     let apply_error_rollback_share_bps =
@@ -5523,7 +5536,7 @@ fn main() -> Result<()> {
         .map(|h| h.missed_proposals)
         .collect();
     println!(
-        "[consensus] finality_avg_ms={} finality_p50_ms={} finality_p95_ms={} finality_max_ms={} scheduler_elapsed_avg_ms={} scheduler_elapsed_p50_ms={} scheduler_elapsed_p95_ms={} scheduler_elapsed_max_ms={} scheduler_share_avg_ppm={} preexec_elapsed_avg_ms={} preexec_elapsed_p50_ms={} preexec_elapsed_p95_ms={} preexec_elapsed_max_ms={} preexec_share_avg_ppm={} commit_elapsed_avg_ms={} commit_elapsed_p50_ms={} commit_elapsed_p95_ms={} commit_elapsed_max_ms={} commit_share_avg_ppm={} state_root_total_avg_ms={} state_root_total_p50_ms={} state_root_total_p95_ms={} state_root_total_max_ms={} state_root_total_share_avg_ppm={} unprofiled_finality_share_bps={} critical_wait_blocks_avg={} critical_wait_blocks_p50={} critical_wait_blocks_p95={} critical_wait_blocks_max={} block_txs_p50={} block_txs_p95={} block_txs_max={} block_groups_p50={} block_groups_p95={} block_groups_max={} avg_group_size_avg_milli={} avg_group_size_p50_milli={} avg_group_size_p95_milli={} avg_group_size_max_milli={} hot_object_share_avg_ppm={} hot_object_share_p50_ppm={} hot_object_share_p95_ppm={} hot_object_share_max_ppm={} rollback_count_avg={} rollback_count_p50={} rollback_count_p95={} rollback_count_max={} rollback_share_avg_ppm={} rollback_peak_share_ppm={} rollback_block_total={} rollback_block_rate={:.6} preexec_reject_total={} apply_error_total={} apply_error_preexec_conflict_miss_total={} preexec_conflict_miss_share_bps={} apply_error_version_conflict_total={} apply_error_invalid_transition_total={} apply_error_deadline_exceeded_total={} apply_error_semantic_fail_total={} rollback_total={} apply_error_rollback_share_bps={} timeout_migrated_total={} recovery_error_rate={:.6} bft_committed_heights={} bft_round_change_total={} bft_round_change_per_height_ppm={} bft_round_change_backoff_total_ms={} bft_round_change_backoff_avg_ms={} bft_round_change_backoff_share_ppm={} bft_leader_missed_proposals={:?} bft_double_vote_total={} bft_auth_reject_bad_sig_total={} bft_auth_reject_replay_total={} bft_auth_reject_stale_nonce_total={}",
+        "[consensus] finality_avg_ms={} finality_p50_ms={} finality_p95_ms={} finality_max_ms={} scheduler_elapsed_avg_ms={} scheduler_elapsed_p50_ms={} scheduler_elapsed_p95_ms={} scheduler_elapsed_max_ms={} scheduler_share_avg_ppm={} preexec_elapsed_avg_ms={} preexec_elapsed_p50_ms={} preexec_elapsed_p95_ms={} preexec_elapsed_max_ms={} preexec_share_avg_ppm={} commit_elapsed_avg_ms={} commit_elapsed_p50_ms={} commit_elapsed_p95_ms={} commit_elapsed_max_ms={} commit_share_avg_ppm={} state_root_total_avg_ms={} state_root_total_p50_ms={} state_root_total_p95_ms={} state_root_total_max_ms={} state_root_total_share_avg_ppm={} unprofiled_finality_share_bps={} critical_wait_blocks_avg={} critical_wait_blocks_p50={} critical_wait_blocks_p95={} critical_wait_blocks_max={} block_txs_p50={} block_txs_p95={} block_txs_max={} block_groups_p50={} block_groups_p95={} block_groups_max={} avg_group_size_avg_milli={} avg_group_size_p50_milli={} avg_group_size_p95_milli={} avg_group_size_max_milli={} hot_object_share_avg_ppm={} hot_object_share_p50_ppm={} hot_object_share_p95_ppm={} hot_object_share_max_ppm={} rollback_count_avg={} rollback_count_p50={} rollback_count_p95={} rollback_count_max={} rollback_share_avg_ppm={} rollback_peak_share_ppm={} rollback_block_total={} rollback_block_rate={:.6} rollback_block_rate_ppm={} preexec_reject_total={} apply_error_total={} apply_error_preexec_conflict_miss_total={} preexec_conflict_miss_share_bps={} apply_error_version_conflict_total={} apply_error_invalid_transition_total={} apply_error_deadline_exceeded_total={} apply_error_semantic_fail_total={} rollback_total={} apply_error_rollback_share_bps={} timeout_migrated_total={} recovery_error_rate={:.6} bft_committed_heights={} bft_round_change_total={} bft_round_change_per_height_ppm={} bft_round_change_backoff_total_ms={} bft_round_change_backoff_avg_ms={} bft_round_change_backoff_share_ppm={} bft_leader_missed_proposals={:?} bft_double_vote_total={} bft_auth_reject_bad_sig_total={} bft_auth_reject_replay_total={} bft_auth_reject_stale_nonce_total={}",
         finality_avg,
         finality_p50,
         finality_p95,
@@ -5575,6 +5588,7 @@ fn main() -> Result<()> {
         rollback_peak_share_ppm,
         rollback_block_total,
         rollback_block_rate,
+        rollback_block_rate_ppm,
         preexec_reject_total,
         apply_error_total,
         apply_error_preexec_conflict_miss_total,
