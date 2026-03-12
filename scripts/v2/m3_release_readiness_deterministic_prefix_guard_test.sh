672947c6c81e4947d3b13bc7b7279fbd79139834
Qian Qi <qianqi@MacBook-Pro.local>
2026-03-12 14:39:24 +0800

    test(release): tighten deterministic rc manifest guard

--- scripts/v2/m3_release_readiness_deterministic_prefix_guard_test.sh
+++ scripts/v2/m3_release_readiness_deterministic_prefix_guard_test.sh
@@ -27,7 +27,19 @@ rc_required_lines=(
   'replay_command=$replay_command'
   'rollback_command=$rollback_command'
   'env_tz=${TZ:-<unset>}'
+  'env_lc_all=${LC_ALL:-<unset>}'
+  'env_lang=${LANG:-<unset>}'
+  'env_source_date_epoch=${SOURCE_DATE_EPOCH:-<unset>}'
+  'env_cargo_term_color=${CARGO_TERM_COLOR:-<unset>}'
+  'env_rust_backtrace=${RUST_BACKTRACE:-<unset>}'
+  'env_cargo_build_jobs=${CARGO_BUILD_JOBS:-<unset>}'
   'replay_env_tz=$replay_tz'
+  'replay_env_lc_all=$replay_lc_all'
+  'replay_env_lang=$replay_lang'
+  'replay_env_source_date_epoch=$replay_source_date_epoch'
+  'replay_env_cargo_term_color=$replay_cargo_term_color'
+  'replay_env_rust_backtrace=$replay_rust_backtrace'
+  'replay_env_cargo_build_jobs=$replay_cargo_build_jobs'
 )
 
 for line in "${rc_required_lines[@]}"; do
