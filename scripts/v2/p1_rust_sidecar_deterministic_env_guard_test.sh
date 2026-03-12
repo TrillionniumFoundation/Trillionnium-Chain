c4fc0a9e2e938f480e1d2cefa0ede417c4fd9e46
Qian Qi <qianqi@MacBook-Pro.local>
2026-03-12 22:30:43 +0800

    lane-L17: harden p1 sidecar deterministic env

--- scripts/v2/p1_rust_sidecar_deterministic_env_guard_test.sh
+++ scripts/v2/p1_rust_sidecar_deterministic_env_guard_test.sh
@@ -10,15 +10,29 @@ if [[ ! -f "$WF" ]]; then
 fi
 
 required_lines=(
-  'timeout-minutes: 45'
+  'group: p1-rust-sidecar-${{ github.workflow }}-${{ github.ref }}'
+  'cancel-in-progress: true'
   'TZ: UTC'
   'LANG: C.UTF-8'
   'LC_ALL: C.UTF-8'
   'LC_NUMERIC: C'
   'LC_COLLATE: C'
+  'LC_TIME: C'
+  'LC_CTYPE: C'
+  'LC_MESSAGES: C'
+  'LC_MONETARY: C'
+  'LC_MEASUREMENT: C'
+  'LC_NAME: C'
+  'LC_PAPER: C'
+  'LC_ADDRESS: C'
+  'LC_TELEPHONE: C'
   'PYTHONHASHSEED: "0"'
-  'HOMEBREW_NO_AUTO_UPDATE: "1"'
+  'PYTHONDONTWRITEBYTECODE: "1"'
+  'PYTHONIOENCODING: "UTF-8"'
+  'PYTHONUTF8: "1"'
   'CI: "true"'
+  'UMASK: "022"'
+  'timeout-minutes: 45'
 )
 
 for line in "${required_lines[@]}"; do
