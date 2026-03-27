37d72120adbe9aab7e46b28ec746851a3ca7ee75
Qian Qi <qianqi@192.168.0.5>
2026-03-28 06:58:57 +0800

    mn08: syntax-check recovery and rollback preflight helpers

--- trillionnium-rust/scripts/testnet_preflight.sh
+++ trillionnium-rust/scripts/testnet_preflight.sh
@@ -97,6 +97,8 @@ fi
 
 log "check shell syntax"
 bash -n ./scripts/testnet_preflight.sh
+bash -n ./scripts/check_bft_restart_recovery.sh
+bash -n ./scripts/release_rc.sh
 bash -n ./scripts/v2/verify_lane_worktree.sh
 bash -n ./scripts/devnet_up.sh
 bash -n ./scripts/devnet_down.sh
