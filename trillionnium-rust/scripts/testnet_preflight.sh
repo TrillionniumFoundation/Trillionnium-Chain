cb388f60a00d52f400d9c45b1d53a4c4364cc55e
Qian Qi <qianqi@192.168.0.5>
2026-03-28 20:09:35 +0800

    mn08: syntax-check industrial readiness scripts in preflight

--- trillionnium-rust/scripts/testnet_preflight.sh
+++ trillionnium-rust/scripts/testnet_preflight.sh
@@ -117,6 +117,8 @@ bash -n ./scripts/testnet_preflight.sh
 bash -n ./scripts/check_bft_restart_recovery.sh
 bash -n ./scripts/release_rc.sh
 bash -n ./scripts/run_local_release_evidence.sh
+bash -n ./scripts/run_industrial_readiness_check.sh
+bash -n ./scripts/check_nightly_green_streak.sh
 bash -n ./scripts/v2/verify_lane_worktree.sh
 bash -n ./scripts/v2/extract_release_handoff_fields.sh
 bash -n ./scripts/devnet_up.sh
