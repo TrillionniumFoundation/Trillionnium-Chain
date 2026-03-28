fbefaf6172434eea77c44ac06dfe74fe184e2dfa
Qian Qi <qianqi@192.168.0.5>
2026-03-28 10:15:43 +0800

    mn08: syntax-check local release evidence script

--- trillionnium-rust/scripts/testnet_preflight.sh
+++ trillionnium-rust/scripts/testnet_preflight.sh
@@ -100,6 +100,7 @@ log "check shell syntax"
 bash -n ./scripts/testnet_preflight.sh
 bash -n ./scripts/check_bft_restart_recovery.sh
 bash -n ./scripts/release_rc.sh
+bash -n ./scripts/run_local_release_evidence.sh
 bash -n ./scripts/v2/verify_lane_worktree.sh
 bash -n ./scripts/v2/extract_release_handoff_fields.sh
 bash -n ./scripts/devnet_up.sh
